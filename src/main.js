3f0d033d42b7a9ac49748d74fb6ccb9b443ae9f4
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2013-06-11 23:10:07 +0900

    backup

--- src/main.js
+++ src/main.js
@@ -3,11 +3,40 @@
  *
  */
 
-
+/**
+ * My Application namespace.
+ * @namespace
+ *
+ */
 var MyApp = MyApp || {};
 
-MyApp.main = function(n) {
-	MyApp.__name__ = n + "mainmain";
-};
+/**
+ * My Application main
+ * @memberOf MyApp
+ *
+ */
+MyApp.main = (function() {
+    /**
+     * Main function of my application.
+     * @param {Number} num of hogehoge.
+     * @return {String} foo text string.
+     */
+    'use strict';
 
+    var C = function(n) {
+        MyApp.__name__ = n + 'mainmain';
+        return MyApp.__name__ + 'hello!';
+    };
 
+    /**
+     * Rows Header class
+     * @constructor
+     * @return {RowHeaderClass}
+     * @see MyApp.main
+     */
+    C.RowsHeader = function() {
+        return { content: null };
+    };
+
+    return C;
+})();
--- src/sub.js
+++ src/sub.js
@@ -3,11 +3,21 @@
  *
  */
 
-
-MyApp = MyApp || {};
-
-MyApp.sub = function(n) {
+/**
+ * Table class lib.
+ * @name WIL.Spreadsheet.Table
+ * @class has Table access methods.
+ * @constructor
+ * @param {Number} n top row
+ * @param {Number} m top left col
+ * @param {Number} x last row
+ * @param {Number} z last right col
+ * @returns {Object} Table class object.
+ *
+ */
+WIL.Table = function(n, m, x, z) {
 	MyApp.__subname__ = n + "subSub";
+	return {name: n + m + x + "table"};
 };
 
 
