52704b5a2de46ee018a6b792d87e1625cf76ad5e
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2013-06-17 09:42:36 +0900

    backup

--- src/main.js
+++ src/main.js
@@ -5,38 +5,36 @@
 
 /**
  * My Application namespace.
- * @namespace MyApp
  *
  */
 var MyApp = MyApp || {};
 
 /**
  * My Application main
- * @memberOf MyApp
  *
  */
 MyApp.main = (function() {
-    /**
-     * Main function of my application.
-     * @param {Number} num of hogehoge.
-     * @return {String} foo text string.
-     */
-    'use strict';
+	/**
+	 * Main function of my application.
+	 * @param {Number} num of hogehoge.
+	 * @return {String} foo text string.
+	 */
+	'use strict';
 
-    var C = function(n) {
-        MyApp.__name__ = n + 'mainmain';
-        return MyApp.__name__ + 'hello!';
-    };
+	var C = function(n) {
+		MyApp.__name__ = n +  d;              //    'mainだぞ。main';
+		return MyApp.__name__ + 9;           //    'hello!   main';
+	};
 
-    /**
-     * Rows Header class
-     * @constructor
-     * @return {RowHeaderClass}
-     * @see MyApp.main
-     */
-    C.RowsHeader = function() {
-        return { content: null };
-    };
+	/**
+	 * Rows Header class
+	 * @constructor
+	 * @return {RowHeaderClass}
+	 * @see MyApp.main
+	 */
+	C.RowsHeader = function() {
+		return { content: null };
+	};
 
-    return C;
+	return C;
 })();
--- src/sub.js
+++ src/sub.js
@@ -5,19 +5,18 @@
 
 /**
  * Table class lib.
- * @name WIL.Spreadsheet.Table
  * @class has Table access methods.
  * @constructor
- * @param {Number} n top row
- * @param {Number} m top left col
- * @param {Number} x last row
- * @param {Number} z last right col
- * @returns {Object} Table class object.
+ * @param {Number} n top row.
+ * @param {Number} m top left col.
+ * @param {Number} x last row.
+ * @param {Number} z last right col.
+ * @return {Object} Table class object.
  *
  */
-WIL.Table = function(n, m, x, z) {
-	MyApp.__subname__ = n + "subSub";
-	return {name: n + m + x + "table"};
-};
+var table = function(n, m, x, z) {
+	MyApp.subname = n + 'subSub';
 
+	return {name: n + m + x + 'table'};
+};
 
