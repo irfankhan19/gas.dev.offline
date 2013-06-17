6db4109281dcff14e728cea11da9be6569d42f46
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2013-06-17 16:35:37 +0900

    backup

--- src/main.js
+++ src/main.js
@@ -14,27 +14,28 @@ var MyApp = MyApp || {};
  *
  */
 MyApp.main = (function() {
-	/**
-	 * Main function of my application.
-	 * @param {Number} num of hogehoge.
-	 * @return {String} foo text string.
-	 */
-	'use strict';
+    /**
+     * Main function of my application.
+     * @param {Number} num of hogehoge.
+     * @return {String} foo text string.
+     */
 
-	var C = function(n) {
-		MyApp.__name__ = n +  d;              //    'mainだぞ。main';
-		return MyApp.__name__ + 9;           //    'hello!   main';
-	};
+    var C = function(n) {
+        var d = 'hello';
+        MyApp.__name__ = n + d;            //    'mainだぞ。main';
+        // return MyApp.__name__ + 9;          //    'hello!   main';
+        return {};
+    };
 
-	/**
-	 * Rows Header class
-	 * @constructor
-	 * @return {RowHeaderClass}
-	 * @see MyApp.main
-	 */
-	C.RowsHeader = function() {
-		return { content: null };
-	};
+    /**
+     * Rows Header class
+     * @constructor
+     * @return {RowHeaderClass}
+     * @see MyApp.main
+     */
+    C.RowsHeader = function() {
+        return { content: null };
+    };
 
-	return C;
+    return C;
 })();
